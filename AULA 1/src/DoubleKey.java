91e5264b964fae2bc0b12a1ee5a9b850547ce5d0
shinaider112 <wendellneander12@gmail.com>
2017-02-23 20:43:01 -0300

    Add files via upload

---
+++ AULA 1/src/DoubleKey.java
@@ -0,0 +1,37 @@
+/**
+ * Created by UniCesumar on 23/02/2017.
+ */
+
+public class DoubleKey {
+
+    private String key1, key2;
+
+    public DoubleKey(){
+        this.key1 = "key1";
+        this.key2 = "key2";
+    }
+
+    public DoubleKey(String key1, String key2){
+        this.key1 = key1;
+        this.key2 = key2;
+    }
+
+    public String getKey1() {
+        return key1;
+    }
+
+    public void setKey1(String key1) {
+        this.key1 = key1;
+    }
+
+    public String getKey2() {
+        return key2;
+    }
+
+    public void setKey2(String key2) {
+        this.key2 = key2;
+    }
+
+
+
+}
---
+++ AULA 1/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by UniCesumar on 23/02/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoubleKey doublekey = new DoubleKey();
+        doublekey.setKey1("chave1");
+        doublekey.setKey2("chave2");
+
+        System.out.println(doublekey.getKey1());
+        System.out.println(doublekey.getKey2());
+    }
+
+}
